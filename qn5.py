3efc2d9f2f6681ed9ae4a7b069bee55fa252dd9b
NITRO5 <gansimarina@gmail.com>
2021-04-15 09:29:30 +0545

    q5

--- qn5.py
+++ qn5.py
@@ -1,2 +1,9 @@
 ''' for given integer x. print 'true'  if it is positive .print 'false' if it is negative and print'zero'
  if ir is 0'''
+num=int(input("enter the number:"))
+if num>0:
+    print("true")
+elif num<0:
+    print("false")
+else:
+    print("zero")
